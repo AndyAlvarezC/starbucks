04cd81390c2d461c4e1c3f3e4073750b2eca345c
CHAIK70 <andykanka@gmail.com>
2025-01-29 12:58:11 +0100

    nav bar created

---
+++ script.js
@@ -0,0 +1,8 @@
+//Actualizar titulo
+function updateTitle() {
+    const currentPage = document.location.pathname.split('/').pop().split('.')[0];
+    const pageName = currentPage === 'index' || currentPage === '' ? 'Home' : 
+                    currentPage.charAt(0).toUpperCase() + currentPage.slice(1);
+    document.title = `${pageName} | Starbucks`;
+}
+window.onload = updateTitle;
